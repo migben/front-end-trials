687ad11ec47f774103b90d0df3b391395834588d
migben <itsmigben@gmail.com>
2020-10-20 05:33:41 -0400

    able to push the new emoji into the array

--- making-it-interactive/emoji-project/index.js
+++ making-it-interactive/emoji-project/index.js
@@ -7,3 +7,13 @@ for (let i = 0; i < myEmojis.length; i++) {
     emoji.textContent = myEmojis[i]
     emojiContainer.append(emoji)
 }
+
+const pushBtn = document.getElementById("push-btn")
+pushBtn.addEventListener("click", function(){
+    const emojiInput = document.getElementById("emoji-input")
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        console.log(myEmojis)    
+    }
+})
