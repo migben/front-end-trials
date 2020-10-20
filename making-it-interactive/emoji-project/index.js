acbac47d203a1c9372635beccffc9536090f211e
migben <itsmigben@gmail.com>
2020-10-20 04:48:41 -0400

    initial setup for the project - added basic html, css and js

---
+++ making-it-interactive/emoji-project/index.js
@@ -0,0 +1,9 @@
+const myEmojis = ["👨‍💻", "⛷", "🍲"]
+
+const emojiContainer = document.getElementById("emoji-container")
+
+for (let i = 0; i < myEmojis.length; i++) {
+    const emoji = document.createElement('span')
+    emoji.textContent = myEmojis[i]
+    emojiContainer.append(emoji)
+}
